36194e24d84874c188799cac6477ea52aa401e91
ToanNguyenP <toan.nguyenp@s3corp.com.vn>
2017-05-24 15:37:09 +0700

    Change framework from unittest to pytest

--- services.py
+++ services.py
@@ -1,9 +1,9 @@
 from urllib.parse import urljoin
 import requests
-import json
 
 from constants import BASE_URL
 
+# Get list of users
 def get_list_of_users():
 	GET_USER_URL = urljoin(BASE_URL, 'users')
 	response = requests.get(GET_USER_URL)
@@ -13,6 +13,7 @@ def get_list_of_users():
 	else:
 		None
 
+# Get user by id
 def get_user_by_id(user_id):
 	GET_USER_URL = urljoin(BASE_URL, 'users/' + user_id)
 	response = requests.get(GET_USER_URL)
@@ -22,15 +23,7 @@ def get_user_by_id(user_id):
 	else:
 		None
 
-def get_user_by_id(user_id):
-	GET_USER_URL = urljoin(BASE_URL, 'users/' + user_id)
-	response = requests.get(GET_USER_URL)
-
-	if (response.ok):
-		return response
-	else:
-		None
-
+#Find user by id
 def find_users_by_id(user_id):
 	FIND_USER_URL = urljoin(BASE_URL, 'users?id=' + user_id)
 	response = requests.get(FIND_USER_URL)
@@ -40,6 +33,7 @@ def find_users_by_id(user_id):
 	else:
 		None
 
+#Find user by name
 def find_users_by_name(name):
 	FIND_USER_URL = urljoin(BASE_URL, 'users?name=' + name)
 	response = requests.get(FIND_USER_URL)
@@ -49,6 +43,7 @@ def find_users_by_name(name):
 	else:
 		None
 
+#Find user by username
 def find_users_by_username(username):
 	FIND_USER_URL = urljoin(BASE_URL, 'users?username=' + username)
 	response = requests.get(FIND_USER_URL)
@@ -58,6 +53,7 @@ def find_users_by_username(username):
 	else:
 		None
 
+# Find user by email
 def find_users_by_email(email):
 	FIND_USER_URL = urljoin(BASE_URL, 'users?email=' + email)
 	response = requests.get(FIND_USER_URL)
@@ -67,6 +63,7 @@ def find_users_by_email(email):
 	else:
 		None
 
+# Create new user
 def create_user(data):
 	CREATE_USER_URL = urljoin(BASE_URL, 'users')
 	response = requests.post(CREATE_USER_URL, data=data)
@@ -76,6 +73,7 @@ def create_user(data):
 	else:
 		None
 
+# Update user
 def update_user(user_id, data):
 	UPDATE_USER_URL = urljoin(BASE_URL, 'users/' + user_id)
 	response = requests.put(UPDATE_USER_URL, data)
@@ -85,6 +83,7 @@ def update_user(user_id, data):
 	else:
 		None
 
+# Delete user
 def delete_user(user_id):
 	DELETE_USER_URL = urljoin(BASE_URL, 'users/' + user_id)
 	response = requests.delete(DELETE_USER_URL)
